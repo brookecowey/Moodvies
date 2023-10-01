ec03b6b4059d1ee52ac40fa74d31723ef3a4a793
brookecowey <brooke.cowey@gmail.com>
2023-10-01 11:46:14 -0500

    added background image

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
 
 import styles from "./Home.css";
 
 export default function Home() {
+
+  
   return (
     <div className={styles.main}>
       <div className={styles.subContainer}>
         <div className={styles.titles}>
           <p>Welcome to Moodvies, your custom mood movie selector!</p>
         </div>
-      
+
       </div>
       <div className={styles.tag}>
         <p>
